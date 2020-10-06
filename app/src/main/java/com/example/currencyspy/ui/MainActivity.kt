462e3444189da65d380b684f88a3fe974d4f964b
artyomvlasov <artyom.vlasov@droidsonroids.pl>
2020-10-06 15:42:00 +0200

    Setup navigation component with empty home fragment

--- app/src/main/java/com/example/currencyspy/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyspy/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.currencyspy
+package com.example.currencyspy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.currencyspy.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
